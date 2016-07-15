f93694067f6679974cdbfcc0938cb90e63e651d4
oliver1094 <futpiz_5678@hotmail.com>
2016-07-15 16:24:45 -0500

    Plantilla Yii2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=restaurant',
+    'username' => 'root',
+    'password' => 'Futbol02069410',
+    'charset' => 'utf8',
+];
